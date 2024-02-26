9519c2dcf6442fd8d8ebadbacc7dac547ed2e8a0
yuek666 <108480435+yuek666@users.noreply.github.com>
2024-02-26 11:36:05 +0800

    Create hw1_2_2.java

---
+++ hw1_2_2.java
@@ -0,0 +1,17 @@
+
+
+
+public class hw1_2_2{
+	public static void main ( String[] args )
+	{
+		System.out.println( "please input Years " ) ;
+		Scanner input = new Scanner( System.in ) ;
+		System.out.println( "please input Months " ) ;
+		System.out.println( "please input Day " ) ;
+		int year = input.nextInt ;
+		int month = input.nextInt ;
+		int day = input.nextInt ;
+	} 
+
+
+}
