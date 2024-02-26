472d1bfa1f8a1bc28a24159ef57d6aa5889f8d6f
yuek666 <108480435+yuek666@users.noreply.github.com>
2024-02-26 11:47:57 +0800

    Update hw1_2_2.java

--- hw1_2_2.java
+++ hw1_2_2.java
@@ -1,4 +1,4 @@
-
+//U11216019 劉宸昕
 
 
 public class hw1_2_2{
@@ -11,11 +11,21 @@ public class hw1_2_2{
 		int year = input.nextInt() ;
 		int month = input.nextInt() ;
 		int day = input.nextInt() ;
+		int sum = 0 ;
 		int[] md = { 31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31 } ;
 		if ( year % 4 == 0 )
 		{
 			md[1] ++ ;
 		}
+		for ( int i = 0 ; i < month ; i ++ )
+		{
+			if ( i + 1 != month )
+			{
+				sum = sum + md[i] ;
+			}	
+		}
+		sum = sum + day ;
+		System.out.println( "day " + sum ) ;
 	} 
 
 
