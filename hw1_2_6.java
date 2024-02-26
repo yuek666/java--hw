d6f23e3f3ed829104c7117e25d9796e74d9f3f63
yuek666 <108480435+yuek666@users.noreply.github.com>
2024-02-26 20:08:59 +0800

    Create hw1_2_6.java
    未完成

---
+++ hw1_2_6.java
@@ -0,0 +1,15 @@
+//U11216019 劉宸昕
+
+public class hw1_2_6{
+    public static void main ( String[] args )
+    {
+        int[] newArr = rightShift( new int[] { 4 , 21 , 4 , 3 , 6 ,7 ,12 , 43 , 11 233 } ,1 ) ;
+        for ( int i : newArr )
+        {
+            System.out.println( i + " " ) ;
+            System.out.println() ;
+            System.out.println("=========================") ;
+            int
+        }
+    }
+}
