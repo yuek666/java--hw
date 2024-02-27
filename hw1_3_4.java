ad327b7d05d4ffa333aa1b039424a7b808e92640
yuek666 <108480435+yuek666@users.noreply.github.com>
2024-02-27 17:00:14 +0800

    題目複製

---
+++ hw1_3_4.java
@@ -0,0 +1,13 @@
+//U11216019 劉宸昕
+
+
+
+public class hw1_3_4{
+    public static void main( String[] args )
+    {
+        System.out.println(factorPrime(125));
+        System.out.println(factorPrime(45512));
+        System.out.println(factorPrime(54887544));
+        System.out.println(factorPrime(966969455));
+    }
+}
